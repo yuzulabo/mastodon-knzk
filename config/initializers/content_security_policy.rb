a3e4cc2b67e61b3b3c042e8406b9297171e51cb9
霧島ひなた <naaba6z7y@gmail.com>
2018-09-03 17:02:04 +0900

    Revert CDN setting

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -3,9 +3,6 @@
 # https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Content-Security-Policy
 
 if Rails.env.production?
-
-  #ここから下まで本番環境まで保留の案件
-
   assets_host = Rails.configuration.action_controller.asset_host || "https://#{ENV['WEB_DOMAIN'] || ENV['LOCAL_DOMAIN']}"
   data_hosts = [assets_host]
 
@@ -18,32 +15,21 @@ if Rails.env.production?
   end
   data_hosts << attachments_host unless attachments_host.nil?
 
-  #ここまで
-
   Rails.application.config.content_security_policy do |p|
+    p.base_uri        :none
+    p.default_src     :none
     p.frame_ancestors :none
-    p.object_src  :none 
-    p.script_src  :self 
-    p.base_uri :none 
-#    p.default_src :self, :https 
-#    p.font_src    :self, :https, :data 
-#    p.img_src     :self, :https, :data 
-#    p.style_src   :self, :https, :unsafe_inline 
-# 
-#  # Specify URI for violation reports 
-#  # p.report_uri "/csp-violation-report-endpoint" 
-
-#    p.script_src      :self, assets_host
-#    p.font_src        :self, assets_host
-#    p.img_src         :self, :https, :data, :blob
-#    p.style_src       :self, :unsafe_inline, assets_host
-#    p.media_src       :self, :data, *data_hosts
-#    p.frame_src       :self, :https
-#    p.worker_src      :self, assets_host
-#    p.connect_src     :self, :blob, Rails.configuration.x.streaming_api_base_url, *data_hosts
-
+    p.script_src      :self, assets_host
+    p.font_src        :self, assets_host
+    p.img_src         :self, :https, :data, :blob
+    p.style_src       :self, :unsafe_inline, assets_host
+    p.media_src       :self, :data, *data_hosts
+    p.frame_src       :self, :https
+    p.worker_src      :self, assets_host
+    p.connect_src     :self, :blob, Rails.configuration.x.streaming_api_base_url, *data_hosts
   end
 end
+
 
 # Report CSP violations to a specified URI
 # For further information see the following documentation:
