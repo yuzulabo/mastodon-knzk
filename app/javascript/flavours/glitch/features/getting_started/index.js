d1f30f5bc131bc6e6b90c1259eb0c71bbd0d3717
Kirishima21 <mastodon@mail.kirishima.cloud>
2019-03-10 23:50:27 +0900

    fix get_start menu contents
    リストのレイアウト修正に合わせてリストの内容に手を加えました

--- app/javascript/flavours/glitch/features/getting_started/index.js
+++ app/javascript/flavours/glitch/features/getting_started/index.js
@@ -165,13 +165,13 @@ export default class GettingStarted extends ImmutablePureComponent {
 
           <div className='getting-started__footer'>
             <ul>
-              {invitesEnabled && <li><a href='/invites' target='_blank'><FormattedMessage id='getting_started.invite' defaultMessage='Invite people' /></a> · </li>}
-              <li><a href='/about/more' target='_blank'><FormattedMessage id='navigation_bar.info' defaultMessage='About this server' /></a> · </li>
-              <li><a href='https://joinmastodon.org/apps' target='_blank'><FormattedMessage id='navigation_bar.apps' defaultMessage='Mobile apps' /></a> · </li>
-              <li><a href='/terms' target='_blank'><FormattedMessage id='getting_started.terms' defaultMessage='Terms of service' /></a> · </li>
-              <li><a href='https://docs.joinmastodon.org' target='_blank'><FormattedMessage id='getting_started.documentation' defaultMessage='Documentation' /></a></li>
-              <li class="description">•&nbsp;<a href='https://thedesk.top' rel='noopener' target='_blank'><FormattedMessage id='getting_started.thedeskshort' defaultMessage='TheDesk' /></a></li>
-              <li class="description">•&nbsp;<a href='https://astarte.thedesk.top' rel='noopener' target='_blank'><FormattedMessage id='getting_started.hima_humanshort' defaultMessage='暇人ランキング' /></a></li>
+              {invitesEnabled && <li><a href='/invites' target='_blank'><FormattedMessage id='getting_started.invite' defaultMessage='Invite people' /></a></li>}
+              <li><a href='/about/more' target='_blank'><FormattedMessage id='navigation_bar.info' defaultMessage='About this server' /></a></li>
+              <br/><li><a href='https://joinmastodon.org/apps' target='_blank'><FormattedMessage id='navigation_bar.apps' defaultMessage='Mobile apps' /></a></li>
+              <br/><li><a href='/terms' target='_blank'><FormattedMessage id='getting_started.terms' defaultMessage='Terms of service' /></a></li>
+              <br/><li><a href='https://docs.joinmastodon.org' target='_blank'><FormattedMessage id='getting_started.documentation' defaultMessage='Documentation' /></a></li>
+              <br/><li><a href='https://thedesk.top' rel='noopener' target='_blank'><FormattedMessage id='getting_started.thedeskshort' defaultMessage='TheDesk' /></a></li>
+              <br/><li><a href='https://astarte.thedesk.top' rel='noopener' target='_blank'><FormattedMessage id='getting_started.hima_humanshort' defaultMessage='暇人ランキング' /></a></li>
             </ul>
 
             <p>
