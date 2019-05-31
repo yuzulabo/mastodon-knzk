b2f1517ac6d83f6349404212cbdbdfefe265b6fa
nzws <git@nzws.me>
2019-05-31 20:36:00 +0900

    Add update script

---
+++ updater.sh
@@ -0,0 +1,21 @@
+echo -e "\e[33m Backup the database: \e[m"
+/usr/pgsql-11/bin/pg_dump mastodon > ../backup/$(date +%Y%m%d_%H-%M-%S).sql
+
+echo -e "\e[33m Get new codes from Git: \e[m"
+git fetch origin
+git reset --hard origin/don.nzws.me
+
+echo -e "\e[33m Update deps: \e[m"
+bundle install -j$(getconf _NPROCESSORS_ONLN) --deployment --without development test
+yarn install
+
+echo -e "\e[33m Compile static files: \e[m"
+RAILS_ENV=production bundle exec rails assets:precompile
+
+echo -e "\e[33m Migrate the database: \e[m"
+RAILS_ENV=production bundle exec rails db:migrate
+
+echo -e "\e[32m Success! \e[m"
+echo -e "Use these commands on sudo user to complete the update:\n"
+echo "sudo systemctl reload mastodon-web"
+echo "sudo systemctl restart mastodon-sidekiq"
