774d4aa703c3855a6c314a28da34a6fb257da221
nzws <dev@nzws.me>
2020-09-30 01:05:06 +0900

    Fix modify

--- app/javascript/mastodon/components/status.js
+++ app/javascript/mastodon/components/status.js
@@ -448,8 +448,6 @@ class Status extends ImmutablePureComponent {
           <div className={classNames('status', `status-${status.get('visibility')}`, { 'status-reply': !!status.get('in_reply_to_id'), muted: this.props.muted, read: unread === false })} data-id={status.get('id')}>
             <div className='status__expand' onClick={this.handleExpandClick} role='presentation' />
             <div className='status__info'>
-              <span className='status__relative-time'><Icon id={visibilityIcon ? visibilityIcon : 'question'} className='fa-fw' /></span>
-
               <a href={status.get('url')} className='status__relative-time' target='_blank' rel='noopener noreferrer'><RelativeTimestamp timestamp={status.get('created_at')} /></a>
               <span className='status__visibility-icon'><Icon id={visibilityIcon.icon} title={visibilityIcon.text} /></span>
 
